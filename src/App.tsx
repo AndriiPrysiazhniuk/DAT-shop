1e74daf81a5b57318e44b78838a70d2fddfc79b2
AndriiPrysiazhniuk <sarutora81@gmail.com>
2025-10-29 00:36:09 +0200

    Started the developing of the shop project / Created a header and added a simple styling to it

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="Container">
+                    <nav className={'Header-menu'}>
+                        <ul className={'Header__menu-list'}>
+                            <div className={'Header__list-item Header__list-item-menu'}>
+                                Про нас
+                                <ul className={'About__us-menu'}>
+                                    <li>Наша команда</li>
+                                    <li>Партнери</li>
+                                    <li>Договір</li>
+                                </ul>
+                            </div>
+                            <li className={'Header__list-item'}>
+                                <a href="">
+                                    Каталог продукції
+                                </a>
+                            </li>
+                            <li className={'Header__list-item'}>
+                                <a href="">
+                                    Оплата і доставка
+                                </a>
+                            </li>
+                            <li className={'Header__list-item'}>
+                                <a href="">
+                                    Партнери
+                                </a>
+                            </li>
+                            <li className={'Header__list-item'}>
+                                <a href="">
+                                    Новини
+                                </a>
+                            </li>
+                            <li className={'Header__list-item'}>
+                                <a href="">
+                                    Контакти
+                                </a>
+                            </li>
+                        </ul>
+                        <div className={'Header-registration'}>
+                            <a href="#">Вхід</a>
+                            <a href="#">Реєстрація</a>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
