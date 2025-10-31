8036103e4f8c7538ed48fb41c618defd3c4bcdd3
AndriiPrysiazhniuk <sarutora81@gmail.com>
2025-10-31 09:21:14 +0200

    updated the code and styles / added footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,70 @@ import {Headercopy} from "./ui/Headercopy";
 
 function App() {
     return (
-        <div className="App">
-            {/*<Header/>*/}
-            <Headercopy/>
-        </div>
+        <>
+            <Header/>
+            {/*<Headercopy/>*/}
+
+            <main className="main">
+                <h1>Hi</h1>
+            </main>
+            <footer className="footer">
+                <div className={'Container footer__inner'}>
+                    <div className={'footer__text-logo'}>
+                        <div className={'footer-logo'}></div>
+                        <p className="footer-text">
+                            Пропонуємо покупцям широкий вибір насіння овочів
+                        </p>
+                    </div>
+                    <ul className={'footer-list footer-info'}>
+                        <li>
+                            Інформація
+                        </li>
+                        <li>
+                            Про компанію
+                        </li>
+                        <li>
+                            Оплата і доставка
+                        </li>
+                        <li>
+                            Партнери
+                        </li>
+                    </ul>
+                    <ul className={'footer-list footer-products'}>
+                        <li>
+                            Товари
+                        </li>
+                        <li>
+                            Каталог продукції
+                        </li>
+                        <li>
+                            Засоби захисту рослин
+                        </li>
+                        <li>
+                            Насіння
+                        </li>
+                        <li>
+                            Добрива
+                        </li>
+                        <li>
+                            Агроному в поміч
+                        </li>
+
+                    </ul>
+                    <ul className={'footer-list footer-contacts'}>
+                        <li>Контакти</li>
+                        <li className={'footer-contacts__feedback'}>
+                            <span>+38 (067) 115 00 58</span>
+                            <span className={'call__me-back'}>Замовити зворотній зв’язок</span>
+                        </li>
+                        <li className={'footer-contacts__email'}>DAT@gmail.com</li>
+                    </ul>
+                </div>
+                <div className={'copyright'}>
+                    © 2022 DAT
+                </div>
+            </footer>
+        </>
     );
 }
 
